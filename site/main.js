bf7e7eb79ac341bcfa060e9bbc3d15bd26599777
Brandon Hsu <brandondhsu@gmail.com>
2019-02-17 07:06:07 -0800

    Updated UI

--- site/main.js
+++ site/main.js
@@ -15,3 +15,25 @@ socket.on('sleep', data => {
 socket.on('smsSent', data => {
     console.log('smsSent', data)
 })
+
+// If possible add to this code so UI is nice and functional
+
+$(document).ready(function() {
+
+  function toggleSidebar() {
+    $(".button").toggleClass("active");
+    $("main").toggleClass("move-to-left");
+    $(".sidebar-item").toggleClass("active");
+  }
+
+  $(".button").on("click tap", function() {
+    toggleSidebar();
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      toggleSidebar();
+    }
+  });
+
+});
