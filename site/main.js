6dcece42ed10d8dcd9455167ef28df0fea42b9d3
Brandon Hsu <brandondhsu@gmail.com>
2019-02-17 12:17:29 -0800

    asd

--- site/main.js
+++ site/main.js
@@ -3,6 +3,8 @@ const socket = io.connect(`http://${document.domain}:${location.port}`)
 socket.on('openEyes', data => {
     console.log('openEyes', data)
 })
+document.getElementById("openeyes").innerHTML = data;
+openeyes=data;
 
 socket.on('closeEyes', data => {
     console.log('closeEyes', data)
